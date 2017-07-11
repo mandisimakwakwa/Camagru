ffebf5d38ca14d93b0907dac84217d9c0a8cec97
mandisimakwakwa <mandisi.makwakwa@gmail.com>
2017-07-11 04:41:41 +0200

    - Layout Architecture Update

--- sources/backEnd/engines/controllers/indexController.js
+++ sources/backEnd/engines/controllers/indexController.js
@@ -1,3 +1,18 @@
+function ft_indexGalleryButton() {
+
+    alert("Gallery Button has been Pressed");
+}
+
+function ft_indexLoginButton() {
+
+    alert("Login Button has been Pressed");
+}
+
+function ft_indexRegisterButton() {
+
+    alert("Register Button has been Pressed");
+}
+
 function ft_indexControllerDebugger() {
 
     console.log("Index On");
--- sources/frontEnd/html/htmlTemplates/index/indexSectionMain.php
+++ sources/frontEnd/html/htmlTemplates/index/indexSectionMain.php
@@ -1,4 +1,29 @@
 <main>
 
-    Main
+    <div class="indexLMainSubDivAClass">
+
+        <button class="indexGalleryButtonClass"
+                style="vertical-align:middle"
+                onclick="ft_indexGalleryButton()"
+        >
+
+            <span>Gallery</span>
+        </button>
+    </div>
+
+    <div class="indexLMainSubDivBClass">
+
+
+        <button class="indexLoginButtonClass"
+                onclick="ft_indexLoginButton()">
+
+            <span>Login</span>
+        </button>
+
+        <button class="indexRegisterButtonClass"
+                onclick="ft_indexRegisterButton()">
+
+            <span>Register</span>
+        </button>
+    </div>
 </main>
