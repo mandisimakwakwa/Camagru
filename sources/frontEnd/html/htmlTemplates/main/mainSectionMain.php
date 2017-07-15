a1e76443db7b562727898c019d554983e79dd67c
mandisimakwakwa <mandisi.makwakwa@gmail.com>
2017-07-15 11:20:34 +0200

    - Main App Button Implementation and Styling Update

--- sources/frontEnd/html/htmlTemplates/main/mainSectionMain.php
+++ sources/frontEnd/html/htmlTemplates/main/mainSectionMain.php
@@ -1,4 +1,18 @@
 <main>
 
-    This is the Main Section
+    <div class="mainMainSubDivAClass">
+
+        <div class="mainSubDivA11Class"></div>
+
+        <div class="mainSubDivA2Class">
+
+            <button class="roundButtonClass"></button>
+            <button class="roundButtonClass"></button>
+            <button class="roundButtonClass"></button>
+            <button class="roundButtonClass"></button>
+        </div>
+
+        <div class="mainSubDivA12Class"></div>
+    </div>
+    <div class="mainMainSubDivBClass">Merge Images Container</div>
 </main>
